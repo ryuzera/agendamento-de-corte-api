ef45a7ff2c31a079a767b83a15190a72e656adbc
ryuzera <ryugfx2@gmail.com>
2024-11-26 10:41:45 -0300

    Instalando webpack server e carregando o html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     target: "web",
@@ -8,5 +9,18 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist")
+        },
+        port: 3000,
+        open: true,
+        liveReload: true,
+    },
+
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "index.html")
+    })]
 }
