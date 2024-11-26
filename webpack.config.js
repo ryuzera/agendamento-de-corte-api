43799db737a7f2dce39bd09d76a7f066cb92d7cf
ryuzera <ryugfx2@gmail.com>
2024-11-26 11:09:07 -0300

    Carregando o HTML e o CSS

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,17 @@ module.exports = {
     },
 
     plugins: [new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "index.html")
-    })]
+            template: path.resolve(__dirname, "index.html"),
+            favicon: path.resolve("src", "assets", "scissors.svg")
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
 }
