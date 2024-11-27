5d447e1d7b6faa7e6d6c072ea58e7f9a4ccf944d
ryuzera <ryugfx2@gmail.com>
2024-11-27 15:16:31 -0300

    Renderizando os agendamentos do dia - DOM

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,6 +1,6 @@
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load";
-
+import { schedulesShow } from "../schedules/show.js"
 // Seleciona o input de data
 const selectedDate = document.getElementById("date")
 
@@ -10,6 +10,9 @@ export async function schedulesDay() {
 
     // Busca na API os agendamentos
     const dailySchedules = await scheduleFetchByDay({ date })
+
+    // Exibe os agendamentos
+    schedulesShow({ dailySchedules })
 
     // Renderiza as horas disponíveis
     hoursLoad({ date })
